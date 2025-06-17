4c7af4f6e883b051a8b5e515597d668d838c0ed6
Abid Pathan <pabid4515@gmail.com>
2025-06-17 21:38:27 +0530

    1st day of JAVA

---
+++ Hello.java
@@ -0,0 +1,19 @@
+class Hello {
+    public static void main(String a[])
+    { 
+        byte by = 12;
+        short sh = 100;
+        int num = 129;
+        long  l = 12345l;
+
+        float dec = 4.5f;
+        double d = 5.8;
+
+        char letter = 'k';
+
+        boolean bool = true;
+
+        
+        System.out.println("hello world");
+    }
+}
