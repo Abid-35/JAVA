ac14e40bc55ddc0ae197a56da38dd4884108562e
Abid Pathan <pabid4515@gmail.com>
2025-07-02 18:04:39 +0530

    Java

--- Arralist.java
+++ Arralist.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 public class Arralist{
     public static void main(String[] args) {
         System.out.println("Welcome for the List ");
@@ -9,9 +10,27 @@ public class Arralist{
         num.add(6);
         num.add(10);
         System.out.println(num);
+        for(int n : num){
+            System.out.println(n);
+        }
 
-        //To acess with index numbers we need to use List not the Collection..
-
-
+        //To acess with index numbers we need to use List not the Collection..   
+        
+        List<Integer> nums = new ArrayList<Integer>();   
+        nums.add(3);
+        nums.add(5);
+        nums.add(9);
+        nums.add(8);
+        System.out.println(nums);
+        System.out.println("The Value at index 2 :" + nums.get(2));
+        System.out.println("The Index Value of 8 :" + nums.indexOf(8));
+        System.out.println("Enhanced For Loop ");
+        for(int n : nums){
+            System.out.println(n);
+        }
+        System.out.println("For Loop");
+        for(int i=0;i<nums.size();i++){
+        System.out.println("The Value at index" + i +":" + nums.get(i));
+        }
         
     }}
