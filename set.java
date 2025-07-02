a40dc8d591b17d4e0bec3504d0bd3018db5fbb24
Abid Pathan <pabid4515@gmail.com>
2025-07-02 21:57:52 +0530

    Java

--- set.java
+++ set.java
@@ -1,6 +1,36 @@
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class set{
     public static void main(String[] args){
         System.out.println("Welcome to Set");
+        Set<Integer> no = new TreeSet<Integer>();
+        no.add(83);
+        no.add(35);
+        no.add(0);
+        no.add(7);
+        no.add(0);
+        System.out.println(no);
+        for(int n : no){
+             System.out.println(n);
+         }
+         System.out.println("Now");
+          Iterator<Integer> now = no.iterator();
+          while (now.hasNext()) { 
+            System.out.println(now.next());
+              
+          }
+
+        
+
+
+
+
+
+
+
         
     }
 }
