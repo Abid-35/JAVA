9e09920016805079a33e2c39034951a977cd6b2a
Abid Pathan <pabid4515@gmail.com>
2025-07-02 17:40:14 +0530

    Java

---
+++ Arralist.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Collection;
+public class Arralist{
+    public static void main(String[] args) {
+        System.out.println("Welcome for the List ");
+        Collection<Integer> num = new ArrayList<Integer>();
+        num.add(2);
+        num.add(4);
+        num.add(6);
+        num.add(10);
+        System.out.println(num);
+
+        //To acess with index numbers we need to use List not the Collection..
+
+
+        
+    }}
