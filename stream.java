da422cf8b9da578a49fa780fd9ed18683ab212ce
Abid Pathan <pabid4515@gmail.com>
2025-07-03 22:46:14 +0530

    Java

--- stream.java
+++ stream.java
@@ -1,29 +1,35 @@
 
 import java.util.Arrays;
 import java.util.List;
-
+import java.util.stream.Stream;
 public class stream{
-    public static void main(String[] args){   
-    	
-    	List<Integer> nums=Arrays.asList(4,5,7,3,2,6);
-    	
-//    	Stream<Integer> s1=nums.stream();
-//    	Stream<Integer> s2= s1.filter(n ->n%2==0);
-//    	Stream<Integer> s3= s2.map(n->n*2);
-//    	int result=s3.reduce(0,(c,e)->c+e);
-//    	
-//    	s2.forEach(n -> System.out.println(n));
-//    	s3.forEach(n -> System.out.println(n));
-//    	
-//    	s1.forEach(n-> System.out.println(n));
-//   	s1.forEach(n-> System.out.println(n));
-    	
-    	int result=nums.stream()
-    					.filter(n-> n%2==0)
-    					.map(n->n*2)
-    					.reduce(0, (c,e)-> c+e);
-    	System.out.println(result);
-    	
-    	
-    }
-}
+	public static void main(String[] args){
+		System.out.println("Welcome to Stream");
+		List<Integer> now = Arrays.asList(2,3,4,5);
+		System.out.println(now);
+		System.out.println("stream");
+		Stream<Integer> no = now.stream();
+		no.forEach(n-> System.out.println(n));
+		System.out.println("Filter");
+		Stream<Integer> fil = now.stream().filter(n->n%2!=0);
+		fil.forEach(n->System.out.println(n));
+		System.out.println("Map");
+		Stream<Integer> filt = now.stream().map(n->n*5);
+		filt.forEach(n->System.out.println(n));
+		System.out.println("Reduce");
+		int result = now.stream().reduce(0,(c,e)->c+e);
+		System.out.println(result);
+   
+        List<Integer> nom = Arrays.asList(1,2,3,4,5,6,7,9);
+		int res = now.stream()
+		             .filter(n->n%2==0)
+					 .map(n->n*2)
+					 .reduce(0,(c,e)->c+e);
+		System.out.println("All at once " +res);			 
+
+
+		
+
+
+ 	}
+ }
